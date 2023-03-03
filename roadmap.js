73a448ed3f985513369cbc2a370d9e952089d057
NBurtsev790231 <nburtsev7922@gmail.com>
2023-03-04 00:17:32 +0300

    roadmap site

---
+++ roadmap.js
@@ -0,0 +1,14 @@
+'use strict'
+window.onload = function() {
+	setTimeout(function() {
+		document.body.classList.add('loaded')
+
+		if (window.matchMedia('(min-width: 992px)').matches) {
+
+			Draggable.create('.gallery', {
+				bounds: 'body',
+				inertia: true
+			})
+		}
+	}, 200)
+}
