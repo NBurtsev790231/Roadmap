f2dd7a2f33a5719c383fe52254628272225feb66
NBurtsev790231 <nburtsev7922@gmail.com>
2023-05-17 17:53:29 +0300

    17.05.23 modified roadmdp site

--- roadmap.js
+++ roadmap.js
@@ -25,6 +25,13 @@ document.getElementById('open-modal-box3').addEventListener('click',function() {
 	window.location.href='./text/photo_3.html';
 })
 
+document.getElementById('open-modal-box4').addEventListener('click',function() {
+	window.location.href='./text/photo_4.html';
+})
+
+document.getElementById('open-modal-box5').addEventListener('click',function() {
+	window.location.href='./text/photo_5.html';
+})
 /*
 let link = document.querySelector('link[rel=import]');
 let content = link.import.querySelector('#photo_1-dm');
