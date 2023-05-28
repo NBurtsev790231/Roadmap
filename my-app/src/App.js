1c4e4e53daa38e3c8272080bb9630a8612b21dfc
NBurtsev790231 <nburtsev7922@gmail.com>
2023-05-28 15:36:10 +0300

    react start 28.05/2

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import './Styles/roadmap.css'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1 className="header">Turkey <span>Loading...</span></h1>
+
+<main className="gallery">
+	<div className="photo__item" id="open-modal-box"><img src="img/1.jpg" alt="alt"/></div>
+	<div className="photo__item" id="open-modal-box2"><img src="img/2.jpg" alt="alt"/></div>
+	<div className="photo__item" id="open-modal-box3"><img src="img/3.jpg" alt="alt"/></div>
+	<div className="photo__item" id="open-modal-box4"><img src="img/4.jpg" alt="alt"/></div>
+	<div className="photo__item" id="open-modal-box5"><img src="img/5.jpg" alt="alt"/></div>
+	<div className="photo__item"><img src="img/6.jpg" alt="alt"/></div>
+	<div className="photo__item"><img src="img/7.jpg" alt="alt"/></div>
+	<div className="photo__item"><img src="img/8.jpg" alt="alt"/></div>
+	<div className="photo__item"><img src="img/9.jpg" alt="alt"/></div>
+	<div className="photo__item"><img src="img/10.jpg" alt="alt"/></div>
+	<div className="photo__item"><img src="img/11.jpg" alt="alt"/></div>
+	<div className="photo__item"><img src="img/12.jpg" alt="alt"/></div>
+	<div className="photo__item"><img src="img/13.jpg" alt="alt"/></div>
+	<div className="photo__item"><img src="img/14.jpg" alt="alt"/></div>
+	<div className="photo__item"><img src="img/15.jpg" alt="alt"/></div>
+	<div className="photo__item"><img src="img/16.jpg" alt="alt"/></div>
+	<div className="photo__item"><img src="img/17.jpg" alt="alt"/></div>
+	<div className="photo__item"><img src="img/18.jpg" alt="alt"/></div>
+	<div className="photo__item"><img src="img/19.jpg" alt="alt"/></div>
+	<div className="photo__item"><img src="img/20.jpg" alt="alt"/></div>
+	<div className="photo__item"><img src="img/21.jpg" alt="alt"/></div>
+	<div className="photo__item"><img src="img/22.jpg" alt="alt"/></div>
+	<div className="photo__item"><img src="img/23.jpg" alt="alt"/></div>
+	<div className="photo__item"><img src="img/24.jpg" alt="alt"/></div>
+	<div className="photo__item"><img src="img/25.jpg" alt="alt"/></div>
+	<div className="photo__item"><img src="img/26.jpg" alt="alt"/></div>
+	<div className="photo__item"><img src="img/27.jpg" alt="alt"/></div>
+	<div className="photo__item"><img src="img/28.jpg" alt="alt"/></div>
+	<div className="photo__item"><img src="img/29.jpg" alt="alt"/></div>
+	<div className="photo__item"><img src="img/30.jpg" alt="alt"/></div>
+	<div className="photo__item"><img src="img/31.jpg" alt="alt"/></div>
+	<div className="photo__item"><img src="img/32.jpg" alt="alt"/></div>
+	<div className="photo__item"><img src="img/33.jpg" alt="alt"/></div>
+	<div className="photo__item"><img src="img/34.jpg" alt="alt"/></div>
+	<div className="photo__item"><img src="img/35.jpg" alt="alt"/></div>
+	<div className="photo__item"><img src="img/36.jpg" alt="alt"/></div>
+	<div className="photo__item"><img src="img/37.jpg" alt="alt"/></div>
+	<div className="photo__item"><img src="img/38.jpg" alt="alt"/></div>
+	<div className="photo__item"><img src="img/39.jpg" alt="alt"/></div>
+	<div className="photo__item"><img src="img/40.jpg" alt="alt"/></div>
+	<div className="photo__item"><img src="img/41.jpg" alt="alt"/></div>
+	<div className="photo__item"><img src="img/42.jpg" alt="alt"/></div>
+	<div className="photo__item"><img src="img/43.jpg" alt="alt"/></div>
+	<div className="photo__item"><img src="img/44.jpg" alt="alt"/></div>
+	<div className="photo__item"><img src="img/45.jpg" alt="alt"/></div>
+	<div className="photo__item"><img src="img/46.jpg" alt="alt"/></div>
+	<div className="photo__item"><img src="img/47.jpg" alt="alt"/></div>
+</main>
+
     </div>
   );
 }
